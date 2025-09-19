99bfaa7531bb994b078f67c3e1ef9833775fffb7
Afzal Khan <afzalkhan101.contact@gmail.com>
2025-09-19 23:44:33 +0600

    add a new file of requrtion with linearshearch

---
+++ linear_search_with_recurtion.py
@@ -0,0 +1,18 @@
+
+def linear_search(li , size , key):
+
+    if size == 0 :
+        return -1 
+    elif li[0] == key:
+        return 0 
+    elif li[size-1] == key:
+        return size -1 
+    
+    else:
+        return linear_search(li ,size-1 , key)
+
+if __name__ =="__main__":
+    li = [1,2,3,4,5]
+    size = 5 
+    key = 10 
+    print(linear_search(li , size , key))
